9157e97fff4c6f0fa40fe042cdd808effea8b5ff
Ainara Saralegui <ainarasaal@gmail.com>
2018-05-15 17:49:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,20 +7,25 @@ return arrays
 
 }
 
-function johnLennonFacts(array){
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]
-  function maybeTrue() {
-    return Math.random() >= 0.5
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
   }
 
-  do {
-    array = array.unshift `!!!`
-  } while (array && maybeTrue())
-  return array
+  return shoutedFacts
+}
 
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
 }
