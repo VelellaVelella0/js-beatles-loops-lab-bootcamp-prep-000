82cc2adfac8f92df36a80defb0cfcba0f0127e58
Ainara Saralegui <ainarasaal@gmail.com>
2018-05-15 17:36:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(arraym, arrayi){
   var arrays=[]
     for (let i = 0; i <arraym.length; i++ ) {
-  arrays.push('${arraym[i]} plays {arrayi[i]}')
+  arrays.push(`${arraym[i]} plays {arrayi[i]}`)
 }
 return arrays
 
